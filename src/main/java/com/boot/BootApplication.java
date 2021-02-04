8c0e10ffded86c4ca090a24b63d0917ab2fb6980
lodwinmoloto <lodwin@anylytical.co.za>
2021-02-04 22:09:56 +0200

    Project directories setup.

---
+++ src/main/java/com/boot/BootApplication.java
@@ -0,0 +1,16 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+    /**
+     * Entry point into the boot application.
+     * @param args
+     */
+    public static void main(String[] args){
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
